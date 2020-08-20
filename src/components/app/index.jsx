3e1b234cc7a47c9703fc2fdddd53b971dc56e411
Grigor Abrahamyan <g.r.abrahamyan@gmail.com>
2020-08-21 02:23:59 +0400

    5

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from '../header';
 import Footer from '../footer';
 import Routes from '../routes';
+import HomePage from '../main/homePage';
 
 function App() {
   return (
     <div className='mainWrapper' >
       <Router >
         <Header />
-        <Routes />
+        <HomePage />
         <Footer />
       </Router>
     </div >
