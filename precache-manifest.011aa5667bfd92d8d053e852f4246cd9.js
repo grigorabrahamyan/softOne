efcce2fd077652109d93a7ca93f7eaa11072792d
Grigor Abrahamyan <g.r.abrahamyan@gmail.com>
2020-08-21 02:05:45 +0400

    Updates

---
+++ precache-manifest.011aa5667bfd92d8d053e852f4246cd9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1753828032c9c5f60bad3aeb702d2dc",
+    "url": "/softOne/index.html"
+  },
+  {
+    "revision": "d16bda272649ac273ab8",
+    "url": "/softOne/static/css/main.e0342e43.chunk.css"
+  },
+  {
+    "revision": "22b72a397268f9f44d90",
+    "url": "/softOne/static/js/2.64c47ae8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/softOne/static/js/2.64c47ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16bda272649ac273ab8",
+    "url": "/softOne/static/js/main.a1599c2b.chunk.js"
+  },
+  {
+    "revision": "320c9f1d5b176bd25cc4",
+    "url": "/softOne/static/js/runtime-main.7fa0a628.js"
+  },
+  {
+    "revision": "c0d9cc5196fd1edb9e752ab0f93315fd",
+    "url": "/softOne/static/media/bestsellers1.c0d9cc51.png"
+  },
+  {
+    "revision": "b548d375891dc9deb9598f98350dd153",
+    "url": "/softOne/static/media/location1.b548d375.png"
+  },
+  {
+    "revision": "54ee86c39bcc6968d3a3253a62d55c3d",
+    "url": "/softOne/static/media/red1.54ee86c3.png"
+  },
+  {
+    "revision": "dc75d2c741ada379bd27e9b9b64605c0",
+    "url": "/softOne/static/media/red2.dc75d2c7.png"
+  },
+  {
+    "revision": "c50d0f4960ee3e004819448c971556c9",
+    "url": "/softOne/static/media/singlCarusel1.c50d0f49.png"
+  },
+  {
+    "revision": "81f0970796f1b233423f5689a195cf4b",
+    "url": "/softOne/static/media/sliderImage2.81f09707.jpg"
+  },
+  {
+    "revision": "38fd5f947356898906d2d67a6dd2c73e",
+    "url": "/softOne/static/media/sliderImage3.38fd5f94.jpg"
+  },
+  {
+    "revision": "2d7f80de5eabbb9c17202d50f5553a09",
+    "url": "/softOne/static/media/sliderImage4.2d7f80de.jpg"
+  },
+  {
+    "revision": "16cdc9de2f1d2745e963b8ffd179b015",
+    "url": "/softOne/static/media/sliderImage6.16cdc9de.jpg"
+  },
+  {
+    "revision": "bd4250a2b82e4025d8c5d7ef138a8867",
+    "url": "/softOne/static/media/sliderImg1.bd4250a2.png"
+  },
+  {
+    "revision": "5d35e19bf66b29291118d27e97f1aa78",
+    "url": "/softOne/static/media/stories1.5d35e19b.png"
+  },
+  {
+    "revision": "eb516e259b385ba9824fb3c56ef84e39",
+    "url": "/softOne/static/media/stories2.eb516e25.png"
+  }
+]);
